cb0001dcd1e8197a086829b77dd7c3abf77fa9e8
Egor Dovgvillo <filimonchik1998@mail.ru>
2022-05-18 22:15:56 +0300

    HomeWork 7

--- lesson-9/main.js
+++ lesson-9/main.js
@@ -1,3 +1,50 @@
+//Функциональный стиль 
+
+function Animal() {
+    this._foodAmount = 50;
+    this.name = name;
+    this._formatFoodAmount = function () {
+        return this._foodAmount + ' гр.';
+    }
+
+    this.dailyNorm = function (amount) {
+        if (!arguments.length) return this._formatFoodAmount();
+
+        if (amount < 30 || amount > 100) {
+            return 'Недопустимое количество корма.';
+        }
+
+        this._foodAmount = amount;
+    };
+
+
+    this.feed = function () {
+        console.log('Насыпаем в миску ' + this.dailyNorm() + ' корма.');
+    };
+}
+function Cat(name) {
+
+    Animal.call(this, arguments);
+
+    var animalFeed = this.feed;
+
+    this.feed = function () {
+        animalFeed.call(this);
+        console.log('Кот доволен ^_^');
+        return this;
+    }
+    this.stroke = function () {
+        console.log('Гладим кота');
+        return this;
+    }
+}
+
+var barsik = new Cat('Барсик');
+console.log(barsik.dailyNorm(66));
+barsik.feed();
+barsik.feed().stroke().stroke();
+// Прототипный стиль
+
 function Animal() {
     debugger
     this._foodAmount = 50;
