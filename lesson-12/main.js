60af4fe856653fd9e8e223130088dd0ddf3f2fd5
Egor Dovgvillo <filimonchik1998@mail.ru>
2022-05-31 09:56:28 +0300

    HomeWork 11

--- lesson-12/main.js
+++ lesson-12/main.js
@@ -20,7 +20,7 @@ function childSearch() {
 
 secondPar.onclick = function () {
     var target = event.target;
-    if (target.tagName = 'HREF') {
+    if (target.tagName = 'A') {
         event.preventDefault()
         alert(event.target);
     }
